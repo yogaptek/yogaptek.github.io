e46266d11b891273aa64056472627b5b16ae9964
yogaptek <40507418+yogaptek@users.noreply.github.com>
2023-11-24 13:43:57 +0700

    Add files via upload

---
+++ kamus/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1700806548,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/masuk-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/findicon-sheet0.png",
+		"images/findicon-sheet1.png",
+		"images/sprite2-sheet0.png",
+		"images/sound-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"style.css",
+		"adapter.js",
+		"jquery.autocomplete.js"
+	]
+}
